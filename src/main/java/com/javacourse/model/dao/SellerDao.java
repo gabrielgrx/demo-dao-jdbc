1d123dd7584088513a008f406bd5f1d5c34f1a38
Gabriel Xavier <gabrielgrx@gmail.com>
2023-01-05 16:33:02 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/main/java/com/javacourse/model/dao/SellerDao.java
@@ -0,0 +1,13 @@
+package com.javacourse.model.dao;
+
+import com.javacourse.model.entities.Seller;
+
+import java.util.List;
+
+public interface SellerDao {
+    void insert(Seller obj);
+    void update(Seller obj);
+    void deleteById(Integer id);
+    Seller findById(Integer id);
+    List<Seller> findAll();
+}
