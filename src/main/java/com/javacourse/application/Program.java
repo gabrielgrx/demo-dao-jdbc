96fe1b7fccd9340305b674a76a9a1acdda7cba7e
Gabriel Xavier <gabrielgrx@gmail.com>
2023-01-05 15:22:11 -0300

    Department entity class

---
+++ src/main/java/com/javacourse/application/Program.java
@@ -0,0 +1,12 @@
+package com.javacourse.application;
+
+import com.javacourse.model.entities.Department;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
+
+    }
+}
