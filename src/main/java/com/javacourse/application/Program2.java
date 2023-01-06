40eeebd630ca5212e5345a5b445d2cd89351d851
Gabriel Xavier <gabrielgrx@gmail.com>
2023-01-05 21:56:48 -0300

    DepartmentDao findAll implementation

--- src/main/java/com/javacourse/application/Program2.java
+++ src/main/java/com/javacourse/application/Program2.java
@@ -4,14 +4,21 @@ import com.javacourse.model.dao.DaoFactory;
 import com.javacourse.model.dao.DepartmentDao;
 import com.javacourse.model.entities.Department;
 
+import java.util.List;
+
 public class Program2 {
     public static void main(String[] args) {
 
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
-        System.out.println("=== TEST 1: seller findById ====");
+        System.out.println("=== TEST 1: Department findById ====");
         Department department = departmentDao.findById(3);
         System.out.println(department);
 
+        System.out.println("\n=== TEST 2: Department findAll and Order by Name ====");
+        List<Department> list = departmentDao.findAll();
+        for (Department dep : list) {
+            System.out.println(dep);
+        }
     }
 }
--- src/main/java/com/javacourse/model/dao/impl/DepartmentDaoJDBC.java
+++ src/main/java/com/javacourse/model/dao/impl/DepartmentDaoJDBC.java
@@ -4,11 +4,13 @@ import com.javacourse.db.DB;
 import com.javacourse.db.DbException;
 import com.javacourse.model.dao.DepartmentDao;
 import com.javacourse.model.entities.Department;
+import com.javacourse.model.entities.Seller;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DepartmentDaoJDBC implements DepartmentDao {
@@ -63,7 +65,31 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public List<Department> findAll() {
-        return null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement(
+                    "SELECT * FROM department "
+                            + "ORDER BY Name");
+
+            rs = st.executeQuery();
+
+            List<Department> list = new ArrayList<>();
+
+            while (rs.next()) {
+                Department dep = instantiateDepartment(rs);
+                list.add(dep);
+            }
+            return list;
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
 
     private Department instantiateDepartment(ResultSet rs) throws SQLException {
